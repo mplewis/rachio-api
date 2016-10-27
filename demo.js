ae87d76c148bb031437649f288260c895eb38f8e
Matthew Lewis <matt@mplewis.com>
2016-10-27 00:16:09 -0600

    feature: water lawn

--- demo.js
+++ demo.js
@@ -1,12 +1,35 @@
 import {Rachio} from './src/rachio'
 
 const apiKey = process.env.RACHIO_API_KEY
+const targetDevice = 'Mister Googly'
+const targetZone = 'Backyard'
 
 async function main () {
+  // Sign into Rachio
   const r = new Rachio(apiKey)
 
-  const result = await r.me()
-  console.log(result)
+  // Get the current user
+  const me = await r.me()
+  console.log(me)
+
+  // Get the named device
+  const device = me.devices.filter(d => d.data.name === targetDevice)[0]
+  console.log(device)
+
+  // Get all device zones
+  const zones = device.zones
+  console.log(
+    zones
+    .map(z => [z.data.zoneNumber, z.data.name])
+    .sort((a, b) => a[0] - b[0])
+  )
+
+  // Get the named zone
+  const zone = zones.filter(z => z.data.name === targetZone)[0]
+  console.log(zone)
+
+  // Water the zone for 5 seconds
+  zone.start(5)
 }
 
 main()
--- src/rachio.js
+++ src/rachio.js
@@ -14,9 +14,47 @@ function req (apiKey, method, path, data) {
         reject(err)
         return
       }
-      resolve(JSON.parse(body))
+      if (!body) {
+        resolve()
+        return
+      }
+      try {
+        resolve(JSON.parse(body))
+      } catch (err) {
+        console.log(body)
+        reject(err)
+      }
     })
   })
+}
+
+class Person {
+  constructor (rachio, data) {
+    this._rachio = rachio
+    this.devices = data.devices.map(d => new SparseDevice(rachio, d))
+    const keys = ['createDate', 'id', 'username', 'fullName', 'email']
+    keys.filter(key => (key in data)).forEach(key => (this[key] = data[key]))
+  }
+}
+
+class SparseDevice {
+  constructor (rachio, data) {
+    this._rachio = rachio
+    this.data = data
+    this.zones = data.zones.map(z => new SparseZone(this._rachio, z))
+  }
+}
+
+class SparseZone {
+  constructor (rachio, data) {
+    this._rachio = rachio
+    this.data = data
+  }
+
+  async start (duration) {
+    const data = {id: this.data.id, duration: duration}
+    return await this._rachio._put('/public/zone/start', data)
+  }
 }
 
 export class Rachio {
@@ -32,6 +70,10 @@ export class Rachio {
     return await this._req('GET', path, data)
   }
 
+  async _put (path, data) {
+    return await this._req('PUT', path, data)
+  }
+
   async _meId () {
     const idContainer = await this._get('/public/person/info')
     const id = idContainer.id
@@ -45,7 +87,8 @@ export class Rachio {
   async me () {
     if (this._me) return this._me
     const id = await this._meId()
-    const person = await this.person(id)
+    const personData = await this.person(id)
+    const person = new Person(this, personData)
     this._me = person
     return this._me
   }
